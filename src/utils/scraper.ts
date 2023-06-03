1c320a6896c73a56463c49ef580ceb2a4e0c0620
Iván García Laverde <ivan@tyba.com.co>
2023-06-03 18:28:52 -0500

    update scraper class: set up

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -1,27 +1,29 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { PromiseTimeout } from './function';
 
-const STOCK_TIMEOUT = 15000; // 15s
+const AXIOS_TIMEOUT = 15000; // 15s
 
-/**
- * This function makes the request to STOOQ API. 
- * @param {R} req Request API to call
- * @param {StockParams[R]} config.params Params to send if needed
- */
-export const makeStooqAPIRequest = async (symbol: string): Promise<void> => {
+export class WebsiteScraper {
 
-    const options: AxiosRequestConfig = {
-        method: 'GET',
-        baseURL: 'https://stooq.com/q/l',
-        headers: { accept: 'application/json' },
-        params: { s: symbol, f: 'sd2t2ohlcvn', e: 'json' },
+    /**
+     * This function makes the axios request to get the data from a website
+     * @param {R} req Request API to call
+     * @param {StockParams[R]} config.params Params to send if needed
+     */
+    private async makeAxiosGETRequest (baseURL: string): Promise<void> {
+
+        const options: AxiosRequestConfig = {
+            method: 'GET',
+            baseURL,
+        };
+
+        try {
+            const response = await PromiseTimeout(axios.request(options), AXIOS_TIMEOUT);
+            /* return (response.data) as StooqDataResponse; */
+        } catch (error) {
+            console.error('STOOQ Data API error: ', error?.response?.data ?? error);
+            throw error;
+        }
     };
+}
 
-    try {
-        const response = await PromiseTimeout(axios.request(options), STOCK_TIMEOUT);
-        /* return (response.data) as StooqDataResponse; */
-    } catch (error) {
-        console.error('STOOQ Data API error: ', error?.response?.data ?? error);
-        throw error;
-    }
-};
